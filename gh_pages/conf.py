aae8993218ebaad42d9f90e40014b2e27aec80b1
dba <babicdd@gmail.com>
2026-07-14 20:55:01 +0800

    gh_page

---
+++ gh_pages/conf.py
@@ -0,0 +1,39 @@
+html_additional_pages = {
+    'index': 'features.html',
+    'showcase': 'showcase.html',
+    'features': 'features.html',
+}
+templates_path = ['_templates']
+source_suffix = ['.txt']
+master_doc = 'contents'
+project = u'Jam.py'
+copyright = u'2022, Jam.py Team'
+author = u'Andrew Yushev & Dean D. Babic'
+language = "en"
+exclude_patterns = [
+    '_build',
+    'requirements.txt'
+]
+pygments_style = 'sphinx'
+todo_include_todos = False
+html_favicon = '_static/favicon.ico'
+html_static_path = ['_templates']
+html_last_updated_fmt = '%b %d, %Y'
+html_show_sourcelink = False
+htmlhelp_basename = 'Jampydocumentationdoc'
+latex_elements = {
+}
+latex_documents = [
+  (master_doc, 'Jampydocumentation.tex', u'Jam.py Documentation',
+   u'Andrew Yushev & Dean D. Babic', 'manual'),
+]
+man_pages = [
+    (master_doc, 'jampydocumentation', u'Jam.py documentation Documentation',
+     [author], 1)
+]
+texinfo_documents = [
+  (master_doc, 'Jampydocumentation', u'Jam.py documentation Documentation',
+   author, 'Jampydocumentation', 'One line description of project.',
+   'Miscellaneous'),
+]
+
